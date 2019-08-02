8c981ceb4f6e9a4268bfb2465084f21b7ead841d
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-08-02 11:55:31 -0300

    dynamically displays auth forms

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
+import Login from './Login/Login';
+
 import classes from './Auth.module.css';
 
 class Auth extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    let form = (
+      <Login />
+    );
+
+    if(this.props.form === "signup") {
+      form = "signup form";
+    }
+
     return(
       <div className={classes.Container}>
         <div className={classes.Box}>
           <div className={classes.ImgContainer}>
           </div>
           <div className={classes.FormContainer}>
-
+            {form}
           </div>
         </div>
       </div>

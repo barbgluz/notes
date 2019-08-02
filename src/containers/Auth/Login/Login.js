30efff4ed75de36496d5b2f30b68c1b69c6627f2
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-08-02 11:54:49 -0300

    adds login component

---
+++ src/containers/Auth/Login/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import '../../../styles/form.css';
+import classes from '../Auth.module.css';
+
+class Login extends Component {
+  render() {
+    return(
+      <div className={classes.Login}>
+        <h1 className={classes.FormTitle}>Login</h1>
+
+        <form className={classes.Form}>
+          <div className={classes.InputGroup + " InputGroup"}>
+            <input
+              className="Input"
+              type="text"
+              id="email"
+              required />
+            <label className="Label" htmlFor="email">Email</label>
+          </div>
+
+          <div className={classes.InputGroup + " InputGroup"}>
+            <input
+              className="Input"
+              type="password"
+              id="password"
+              required />
+            <label className="Label" htmlFor="password">Password</label>
+          </div>
+
+          <button className={classes.Btn + " Btn"}>Login</button>
+        </form>
+
+        <div className={classes.Footer}>
+          <p>Not a member yet? <a href="#">Sign In</a> </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;

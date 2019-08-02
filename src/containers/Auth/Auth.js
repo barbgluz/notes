769e5666cef800d65d3a768676d9ecf94cad8708
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-08-02 02:30:46 -0300

    adds auth component

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import classes from './Auth.module.css';
+
+class Auth extends Component {
+
+  render() {
+    return(
+      <div className={classes.Container}>
+        <div className={classes.Box}>
+          <div className={classes.ImgContainer}>
+          </div>
+          <div className={classes.FormContainer}>
+
+          </div>
+        </div>
+      </div>
+      );
+  }
+}
+
+export default Auth;

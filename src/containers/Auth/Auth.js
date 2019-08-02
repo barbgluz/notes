c136f48077ff68809c8332c32f39f870c6e79c01
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-08-02 13:24:16 -0300

    adds login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
 
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
@@ -9,10 +11,43 @@ class Auth extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      loginForm: {
+        email: "",
+        password: ""
+      },
+      signupForm: {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirm: ""
+      }
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let formName = event.target.form.id;
+    let form = {...this.state[formName]}
+    let key = event.target.id;
+
+    form[key] = event.target.value;
+
+    this.setState({ [formName]: form });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onAuth(this.state.loginForm.email, this.state.loginForm.password);
   }
 
   render() {
-    let form = <Login />;
+    let form = (<Login
+                  changed={this.handleChange}
+                  submit={this.handleSubmit}/>);
 
     if(this.props.form === "signup") {
       form = <Signup />;
@@ -32,4 +67,16 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+        isAuthenticated: state.auth.token !== null
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);

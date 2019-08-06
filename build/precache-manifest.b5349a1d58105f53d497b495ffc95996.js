6ca06c5e4f8954b4b43653adb6eb6b6d95b6a3d7
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-08-05 23:37:45 -0300

    adds build

---
+++ build/precache-manifest.b5349a1d58105f53d497b495ffc95996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05c84397e2e14e87623d7392edf3b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5fc5094a62ae186cd2d",
+    "url": "/static/css/2.41690672.chunk.css"
+  },
+  {
+    "revision": "e42eb842752425a93f44",
+    "url": "/static/css/main.26a3e482.chunk.css"
+  },
+  {
+    "revision": "b5fc5094a62ae186cd2d",
+    "url": "/static/js/2.dd74e415.chunk.js"
+  },
+  {
+    "revision": "e42eb842752425a93f44",
+    "url": "/static/js/main.0daf2c02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
